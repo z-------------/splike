a5f2d537cd2d253d999520dc91c547c4db43e5f6
Zack Guard <zacharyguard@gmail.com>
2020-07-20 03:31:37 +0800

    Refactor evaluate

--- interpreter/evaluator.js
+++ interpreter/evaluator.js
@@ -18,59 +18,75 @@ module.exports = class Evaluator {
         // throw new ReferenceError(`Unknown identifier '${x}'.`);
         return undefined;
     }
+
+    evalListlike(node, scope) {
+        let values;
+        const head = this.evaluate(node.items[0], scope);
+        if (node.type === NodeType.List && this.macros.includes(head)) {
+            values = [head, ...slice(node.items, 1)];
+        } else {
+            values = [head, ...node.items.slice(1).map(y => this.evaluate(y, scope))];
+        }
+        if (node.type === NodeType.List) {
+            if (!(head instanceof Function)) {
+                throw `'${node.items[0]}' is not a function.`;
+            }
+            return head(scope, ...slice(values, 1));
+        } else {
+            return values;
+        }
+    }
+
+    evalFieldAccess(node) {
+        if (node[1] === "-") {
+            const fieldName = node.substring(2);
+            return function(_, obj) {
+                return obj[fieldName];
+            }
+        } else {
+            const methodName = node.substring(1);
+            return function(_, obj, ...args) {
+                return obj[methodName](...args);
+            };
+        }
+    }
+
+    evalGlobalAccess(node) {
+        const split = node.split("/");
+        let obj = globalThis;
+        for (const name of slice(split, 0, split.length - 1)) {
+            obj = obj[name];
+        }
+        const memberName = split[split.length - 1];
+        return function(_, ...args) {
+            return obj[memberName](...args);
+        };
+    }
+
+    evalHash(node, scope) {
+        const entries = [];
+        for (const [key, val] of node.pairs) {
+            entries.push(key, this.evaluate(val, scope));
+        }
+        return new Hash(entries);
+    }
     
     evaluate(node, scope = {}) {
         // console.log(node, scope);
         if ([NodeType.String].includes(node.type)) {
             return node.literal;
         } else if ([NodeType.List, NodeType.QuotedList, NodeType.Vector].includes(node.type)) {
-            let values;
-            const head = this.evaluate(node.items[0], scope);
-            if (node.type === NodeType.List && this.macros.includes(head)) {
-                values = [head, ...slice(node.items, 1)];
-            } else {
-                values = [head, ...node.items.slice(1).map(y => this.evaluate(y, scope))];
-            }
-            if (node.type === NodeType.List) {
-                if (!(head instanceof Function)) {
-                    throw `'${node.items[0]}' is not a function.`;
-                }
-                return head(scope, ...slice(values, 1));
-            } else {
-                return values;
-            }
+            return this.evalListlike(node, scope);
         } else if (typeof node === "string") {
             if (node.startsWith(".")) {
-                if (node[1] === "-") {
-                    const fieldName = node.substring(2);
-                    return function(_, obj) {
-                        return obj[fieldName];
-                    }
-                } else {
-                    const methodName = node.substring(1);
-                    return function(_, obj, ...args) {
-                        return obj[methodName](...args);
-                    };
-                }
+                return this.evalFieldAccess(node);
             } else if (jsGlobalPat.test(node)) {
-                const split = node.split("/");
-                let obj = globalThis;
-                for (const name of slice(split, 0, split.length - 1)) {
-                    obj = obj[name];
-                }
-                const memberName = split[split.length - 1];
-                return function(_, ...args) {
-                    return obj[memberName](...args);
-                };
+                return this.evalGlobalAccess(node);
             } else {
                 return this.getVal(node, scope);
             }
         } else if (node.type === NodeType.Hash) {
-            const entries = [];
-            for (const [key, val] of node.pairs) {
-                entries.push(key, this.evaluate(val, scope));
-            }
-            return new Hash(entries);
+            return this.evalHash(node, scope);
         } else {
             return node;
         }
